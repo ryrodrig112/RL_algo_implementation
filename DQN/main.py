2fadcf62e73de7006f086d9232137fbdf100c6bd
ryrodrig112 <ryrodrig112@gmail.com>
2022-08-19 00:59:22 -0400

    Implement episode reward tracking

--- DQN/main.py
+++ DQN/main.py
@@ -3,15 +3,26 @@ import gym
 import agent
 
 env_name = "CartPole-v1"
-env = gym.make(env_name)
+env = gym.make(env_name, render_mode = 'human')
 print("Observation space:", env.observation_space)
 print("Action space:", env.action_space)
 
 agent = agent.Agent(env)
 state = env.reset()
 
+best_perf = 0
+ep_perf = 0
+# while best_perf < 200:
+
 for _ in range(200):
-    action = env.action_space.sample()
     action = agent.get_action(state)
-    state, reward, done, info = env.step(action)
-    env.render()
+    state, step_reward, done, info = env.step(action)
+    ep_perf += step_reward
+
+if done:
+    print(ep_perf)
+    env.reset()
+    ep_perf = 0
+
+# if ep_perf > best_perf:
+#     best_perf = ep_perf
--- hill_climbing/agent.py
+++ hill_climbing/agent.py
@@ -3,9 +3,14 @@ import numpy
 class Agent:
     def __init__(self, env):
         self.action_size = env.action_space.n
-        print("Action size:", self.action_size)
+        self.weights = ...
 
     def get_action(self, state):
         pole_angle = state[2]
         action = 0 if pole_angle < 0 else 1
         return action
+
+    def get_update_weights(self, state):
+
+        new_weights = ...
+        self.weights = new_weights
