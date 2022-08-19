36c4fb9be63060fb8252ff8a81b1208adb8f924f
ryrodrig112 <ryrodrig112@gmail.com>
2022-08-19 15:58:53 -0400

    Implement serial trials

--- hill_climbing/agent.py
+++ hill_climbing/agent.py
@@ -9,6 +9,10 @@ class Agent:
         self.action_size = env.action_space.n
         self.noise_amplitude = 1e-2
         self.init_model()
+        self.max_score = 500
+        self.perf_history = []
+        self.noise_history = []
+        self.weight_history = []
 
     def init_model(self):
         self.current_weights = 1e-4 * np.random.rand(*self.state_dim, self.action_size)
@@ -20,12 +24,24 @@ class Agent:
         action = np.argmax(expectation_vector)
         return action
 
+    def update_history(self, ep_perf):
+        self.noise_history.append(self.noise_amplitude)
+        self.weight_history.append(self.current_weights)
+        self.perf_history.append(ep_perf)
+
     def update_weights(self, ep_perf):
+        # if ep_perf >= best_perf: replace best weights and halve noise
+        # if ep_perf < best_perf: keep best weights and double noise amplitude
+        # if we have scored max for the first time, lower noise to set amount
+        # if we scored it consecutively, begin halving
         if ep_perf >= self.best_perf:
             self.best_perf = ep_perf
             self.best_weights = self.current_weights
-            self.noise_amplitude = max(self.noise_amplitude / 2, 1e-3)
+            self.noise_amplitude = max(self.noise_amplitude / 2, 1e-6)
+            if ep_perf == self.max_score:
+                self.noise_amplitude = self.noise_amplitude / 4
         else:
             self.noise_amplitude = min(self.noise_amplitude * 2, 2)
-        self.current_weights = self.best_weights + self.noise_amplitude * np.random.rand(*self.state_dim,
-                                                                                         self.action_size)
+        self.current_weights = self.best_weights + (self.noise_amplitude * np.random.rand(*self.state_dim,
+                                                                                         self.action_size))
+
--- hill_climbing/main.py
+++ hill_climbing/main.py
@@ -12,19 +12,47 @@ state = env.reset()
 best_perf = 0
 attempts = 0
 
+num_trials = 10000
 num_episodes = 100
+goal_consecutive_max = 3
+num_consecutive_max = 0
 
-for episode in range(num_episodes):
-    ep_perf = 0
-    state = env.reset()
-    done = False
-    while not done:
-        action = agent.get_action(state)
-        state, step_reward, done, info = env.step(action)
-        ep_perf += step_reward
-    if ep_perf > best_perf:
-        best_perf = ep_perf
-    print("Attempt: {}, Performance: {}, Best Performance:{}, Noise: {}".format(episode, ep_perf, best_perf, agent.noise_amplitude))
-    agent.update_weights(ep_perf)
+num_eps_in_trial = []
+trial_weights_tracker = []
+trial_noise_tracker = []
+trial_perf_tracker = []
+
+
+for trials in range_num(trials):
+    agent = agent.Agent(env)
+    for episode in range(num_episodes):
+        ep_perf = 0
+        state = env.reset()
+        done = False
+        while not done: # agent applies policy to environment step, takes next step, until episode ends
+            action = agent.get_action(state)
+            state, step_reward, done, info = env.step(action)
+            ep_perf += step_reward
+        # Once the episode ends...
+        # Update tracking metrics (perfs, best_perf, num_consecutive_max)
+        # Print performance
+        # Update agent
+        agent.update_history(ep_perf)
+        if ep_perf == 500:
+            num_consecutive_max += 1
+            if num_consecutive_max > goal_consecutive_max:
+                break
+        else:
+            num_consecutive_max = 0
+        print("Attempt: {}, Performance: {}, Best Performance:{}, Consecutive 500's:{}, Noise: {}"
+              .format(episode, ep_perf, agent.best_perf, num_consecutive_max,agent.noise_amplitude))
+        agent.update_weights(ep_perf)
+    num_eps_in_trial.append(episode)
+    trial_weights_tracker.append(agent.weight_history)
+    trial_noise_tracker.append(agent.noise_history)
+    trial_reward_tracker.append(agent.perf_history)
+
+
+
 
 
